fd0a35cb83198dd75d3466fb35eb2c4e1e88b37a
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-08 22:07:50 +0100

    Homework

--- MsSQL_kozmetickisalon_db.sql
+++ MsSQL_kozmetickisalon_db.sql
@@ -1,30 +1,30 @@
---create database kozmeticki;
-use kozmeticki;
+----create database kozmeticki;
+--use kozmeticki;
 
-create table posjeta(
-korisnik varchar(50),
-termin datetime, 
-djelatnik varchar(50),
-usluga varchar(50)
-);
+--create table posjeta(
+--korisnik varchar(50),
+--termin datetime, 
+--djelatnik varchar(50),
+--usluga varchar(50)
+--);
 
-create table korisnici (
-brmobitel varchar(50),
-spol char(1),
-tipkoze varchar(50),
-dob int, 
-napomena varchar(100)
-);
+--create table korisnici (
+--brmobitel varchar(50),
+--spol char(1),
+--tipkoze varchar(50),
+--dob int, 
+--napomena varchar(100)
+--);
 
-create table djelatnici (
-ID int,
-ime varchar (50),
-prezime varchar(50),
-specijaliziranza varchar(50)
-);
+--create table djelatnici (
+--ID int,
+--ime varchar (50),
+--prezime varchar(50),
+--specijaliziranza varchar(50)
+--);
 
-create table usluga (
-ID int,
-naziv varchar (50),
-cijena decimal (10,2)
-);
+--create table usluga (
+--ID int,
+--naziv varchar (50),
+--cijena decimal (10,2)
+--);
---
+++ MsSQL_udrugazazastituzivotinja.sql
@@ -0,0 +1,33 @@
+----create database udrugazazastituzivotinja;
+
+--use udrugazazastituzivotinja;
+
+--create table prostori(
+--ID int,
+--naziv varchar(50),
+--tip varchar(50),
+--povrsina decimal (18,2),
+--sticenik varchar(50) 
+--);
+
+--create table sticenici (
+--ID int,
+--ime varchar(50),
+--vrsta varchar(50),
+--cijepljen datetime 
+--);
+
+--create table radnici (
+--ID int, 
+--ime varchar(50),
+--prezime varchar(50),
+--oib char(11),
+--email varchar(100),
+--brojmobitela varchar(20)
+--);
+
+--create table radnizadatak(
+--sticenik varchar(50),
+--prostor varchar(50),
+--radnik varchar(50)
+--);
