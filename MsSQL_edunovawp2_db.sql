ec5b3bfbdd435449bc257d1c617ade1cad0377b6
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-08 20:14:08 +0100

    Create MsSQL_edunovawp2_db.sql
    MsSQL practice

---
+++ MsSQL_edunovawp2_db.sql
@@ -0,0 +1,44 @@
+--create database edunovawp2;
+
+use edunovawp2; 
+
+--create table smjerovi(
+--sifra int,
+--naziv varchar(50),
+--brojsati int, 
+--cijena decimal(18,2),
+--upisnina decimal(18,2),
+--verifikacija bit
+--);
+
+--create table grupe (
+--sifra int,
+--naziv varchar(50),
+--predavac varchar(50),
+--smjer varchar(50),
+--makspolaznika int,
+--datumpocetka datetime
+--);
+
+--create table predavaci(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50),
+--oib char(11),
+--email varchar(100),
+--iban varchar(30)
+--);
+
+--create table polaznici (
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50),
+--oib char(11),
+--email varchar(100),
+--brojugovora varchar(20)
+--);
+
+--create table clanovi(
+--grupa varchar(50),
+--polaznik varchar(50)
+--);
