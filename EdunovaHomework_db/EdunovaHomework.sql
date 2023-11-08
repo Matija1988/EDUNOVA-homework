c3f2d26313aeedb501edf689dd195d35109a86b5
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-08 21:15:33 +0100

    Homework db

---
+++ EdunovaHomework_db/EdunovaHomework.sql
@@ -0,0 +1,8 @@
+--create database frizerskisalon;
+
+use frizerskisalon;
+
+create table posjeta(
+korisnik varchar(50),
+
+);
---
+++ MsSQL_frizerskisalon_db.sql
@@ -0,0 +1,27 @@
+--use frizerskisalon;
+
+--create table posjeta (
+--korisnik varchar(50),
+--termin datetime, 
+--djelatnik varchar(50),
+--usluga varchar(50)
+--);
+
+--create table djelatnici (
+--ID int, 
+--ime varchar(50),
+--prezime varchar(50),
+
+--);
+
+--create table korisnici (
+--brojmobitela varchar(50),
+--spol char(1), 
+--tipkose varchar(50)
+--); 
+
+--create table usluga (
+--ID int, 
+--naziv varchar(50),
+--cijena decimal (10,2)
+--);
