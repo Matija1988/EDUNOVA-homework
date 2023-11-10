785730fd5048b493c3eb1964a3ee8678ae962c66
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-10 18:28:34 +0100

    Update MsSQL_vrtic_db.sql

--- MsSQL_vrtic_db.sql
+++ MsSQL_vrtic_db.sql
@@ -1,38 +1,9 @@
---create database vrtic;
-
 use  vrtic;
 
-create table odgojnaskupina (
-ID int,
-grupa varchar(50),
-prostorija varchar(50),
-odgajateljica varchar(50)
+alter table grupe
+add  djete varchar (50); 
 
-);
 
-create table grupe(
-ID int,
-naziv varchar(50), 
-djete varchar(50)
-);
+alter table djeca add skupina varchar(50);
 
-create table djeca (
-ID int,
-ime varchar(50),
-prezime varchar(50),
-spol bit,
-imeOca varchar(50),
-prezimeOca varchar(50),
-imeMajke varchar(50),
-prezimeMajke varchar(50)
-
-);
-
-create table odgajateljice (
-strucnasprema varchar(50),
-ime varchar(50),
-prezime varchar(50),
-oib char(11), 
-iban varchar(50)
-
-);
+alter table grupe add skupina varchar(50); 
