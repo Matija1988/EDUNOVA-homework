f55fc9cfa9308483014a7f6477bc9685f3bfe7eb
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-11 20:59:29 +0100

    Create MsSQL_doktorskaOrdinacija_db.sql

---
+++ MsSQL_doktorskaOrdinacija_db.sql
@@ -0,0 +1,44 @@
+--create database doktorskaOrdinacija;
+
+use doktorskaOrdinacija;
+
+--create table pacijenti(
+--id int, 
+--ime varchar(50),
+--prezime varchar(50),
+--oib char (11),
+--spol char (1),
+--brojMedOsiguranja varchar(50) 
+--);
+
+--create table doktor (
+--id int, 
+--ime varchar(50),
+--prezime varchar(50),
+--specijalizacija varchar(50)
+--);
+
+--create table medSestre(
+--id int, 
+--ime varchar(50),
+--prezime varchar(50),
+--oib char(11)
+--);
+
+--create table timovi(
+--medSestra varchar(50),
+--pacijent varchar(50)
+--);
+
+--create table lijecenje(
+--id int, 
+--brojMedOsiguranja varchar (50), 
+--dijagnoza varchar(100),
+--doktor varchar(50),
+--tim varchar(50)
+
+--);
+
+--drop table timovi;
+
+--alter table medSestre add tim varchar(50);
