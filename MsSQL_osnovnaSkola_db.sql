5d1a2cdcceb53a3a2ee5e00e460d5e443852c673
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-12 17:07:18 +0100

    Homework

---
+++ MsSQL_osnovnaSkola_db.sql
@@ -0,0 +1,35 @@
+--create database osnovnaskola; 
+use osnovnaskola;
+
+--create table djeca(
+--id int, 
+--ime varchar(50),
+--prezime varchar(50),
+--spol char(1)
+--);
+
+--create table uciteljice(
+--id int, 
+--ime varchar(20),
+--prezime varchar(20),
+
+--);
+
+--create table radionice(
+--id int, 
+--naziv varchar(50),
+--tema varchar(50),
+--grupa varchar(50),
+--uciteljica varchar(50)
+--); 
+
+--create table grupa(
+--id int, 
+--naziv varchar(50),
+--dijete varchar(50),
+--radionica varchar(50)
+--);
+
+--alter table grupa drop column radionica;
+
+--alter table djeca add razred varchar(50); 
---
+++ MsSQL_srednjaskola_db.sql
@@ -0,0 +1,27 @@
+--create database srednjaskola; 
+
+use srednjaskola;
+
+--create table razredi(
+--id int, 
+--naziv varchar(50),
+--ucenik varchar(50)
+--);
+
+--create table ucenici(
+--id int, 
+--ime varchar(50),
+--prezime varchar(50)
+--);
+
+--create table profesori(
+--id int, 
+--ime varchar(50),
+--prezime varchar(50),
+--predmet varchar(50)
+--);
+
+--create table grupe(
+--razred varchar(50),
+--profesor varchar(50)
+--);
