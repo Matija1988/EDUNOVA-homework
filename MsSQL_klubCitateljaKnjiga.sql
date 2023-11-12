759afe647cd9aabc80fc30725c293509fe567942
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-12 20:10:13 +0100

    Create MsSQL_klubCitateljaKnjiga.sql

---
+++ MsSQL_klubCitateljaKnjiga.sql
@@ -0,0 +1,38 @@
+--create database klubcitateljaknjiga;
+
+use klubcitateljaknjiga;
+
+--create table citatelji(
+--id int, 
+--serijskiBrojIskaznice int,
+--ime varchar(20),
+--prezime varchar(20),
+--evidencijaPosudbe varchar(50)
+--);
+
+--create table vlasnici(
+--id int,
+--citatelj varchar(50),
+--evidencijaVlasnistva varchar(50)
+--);
+
+--create table knjige(
+--id int,
+--barkodBroj int,
+--naziv varchar(50),
+--autor varchar(50),
+--cijena decimal (6,2)
+--);
+
+--create table evidencijaVlasnistva(
+--vlasnik varchar(50),
+--knjiga varchar(50)
+--);
+
+--create table evidencijaPosudivanja(
+--citatelj varchar(50),
+--serijskiBrojIskaznice int,
+--knjiga varchar(50),
+--datumPosudivanja datetime
+--);
+
