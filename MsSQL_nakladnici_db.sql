46c503572762cbe67d510427af78da70eb681cb5
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-12 19:29:34 +0100

    Create MsSQL_nakladnici_db.sql

---
+++ MsSQL_nakladnici_db.sql
@@ -0,0 +1,35 @@
+--create database nakladnici;
+
+use nakladnici;
+
+--create table nakladnici (
+--id int, 
+--naziv varchar(50),
+--lokacija varchar(50),
+--ponuda varchar(50)
+
+--);
+
+--create table lokacije(
+--nakladnik varchar(50),
+--mjesto varchar(50)
+--);
+
+--create table mjesto(
+--adresa varchar(50),
+--naziv varchar(50),
+--koordinati varchar(50)
+--);
+
+--create table djela(
+--id int, 
+--naziv varchar(50),
+--autor varchar(50),
+--cijena decimal(6,2)
+--);
+
+--create table ponuda(
+--nakladnik varchar(50),
+--djelo varchar(50)
+--);
+
