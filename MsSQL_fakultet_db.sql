56cb0a179e269f297c082fa40a9a852797ad6ce2
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-12 16:28:26 +0100

    Create MsSQL_fakultet_db.sql

---
+++ MsSQL_fakultet_db.sql
@@ -0,0 +1,29 @@
+--create database fakultet; 
+
+use fakultet; 
+
+--create table studenti (
+--id int, 
+--ime varchar (20),
+--prezime varchar(20),
+--brStudomat varchar(20) 
+--);
+
+--create table kolegiji(
+--id int, 
+--naziv varchar(50),
+--pocetak datetime,
+--kraj datetime
+--);
+
+--create table rokovi (
+--id int, 
+--vrijemeOdrzavanja datetime,
+--kolegij varchar(50)
+--);
+
+--create table prijavaNaIspit(
+--id int, 
+--student varchar(50),
+--rok varchar(50)
+--);
