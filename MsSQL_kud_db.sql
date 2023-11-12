d84d07ec4ff6d6e080a90cf4a08cba12ed477dad
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-12 19:44:57 +0100

    Create MsSQL_kud_db.sql

---
+++ MsSQL_kud_db.sql
@@ -0,0 +1,34 @@
+--create database kud;
+
+use kud;
+
+--create table nastupi(
+--id int, 
+--mjesto varchar(50),
+--naziv varchar(50),
+--skupina varchar(50)
+--);
+
+--create table mjesta(
+--adresa varchar(50),
+--naziv varchar(50),
+--koordinati varchar(50),
+--inozemstvo bit
+--);
+
+--create table clanovi(
+--id int, 
+--ime varchar(50),
+--prezime varchar(50),
+--oib char(11),
+--datumIstekaPutovnice datetime,
+--iban varchar(50)
+--);
+
+--create table skupina(
+--clan varchar(50),
+--nastup varchar(50)
+--);
+
+
+
