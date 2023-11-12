636616033facb76108886e50f612063453592bc6
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-12 19:52:22 +0100

    Create MsSQL_djecjaigraonica_db.sql

---
+++ MsSQL_djecjaigraonica_db.sql
@@ -0,0 +1,32 @@
+--create database djecjaigraonica;
+
+use djecjaigraonica;
+
+--create table skupine(
+--naziv varchar(50),
+--grupa varchar(50),
+--soba varchar(50),
+--teta varchar(50)
+--);
+
+--create table djeca(
+--id int,
+--ime varchar(50),
+--prezime varchar(50),
+--imeRoditelja varchar(50),
+--prezimeRoditelja varchar(50),
+--vrijemeDolaska datetime
+
+--);
+
+--create table grupe(
+--radionica varchar(50),
+--djete varchar(50)
+--);
+
+--create table tete(
+--id int, 
+--ime varchar(50),
+--prezime varchar(50),
+--oib char(11)
+--);
