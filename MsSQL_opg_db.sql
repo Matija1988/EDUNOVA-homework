89f65bb41b9db3811b7e743fa4c6a4f496c004af
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-12 20:11:03 +0100

    Update MsSQL_opg_db.sql

--- MsSQL_opg_db.sql
+++ MsSQL_opg_db.sql
@@ -2,29 +2,29 @@
 
 use opg;
 
-create table sirovine(
-id int, 
-naziv varchar(50),
-cijena decimal (12,2)
-);
+--create table sirovine(
+--id int, 
+--naziv varchar(50),
+--cijena decimal (12,2)
+--);
 
-create table postupci (
-sirovina varchar(50),
-proizvod varchar(50),
-djelatnik varchar(50)
-);
+--create table postupci (
+--sirovina varchar(50),
+--proizvod varchar(50),
+--djelatnik varchar(50)
+--);
 
-create table proizvodi(
-id int,
-naziv varchar(50), 
-postupak varchar(50),
-cijena decimal (12,2)
-);
+--create table proizvodi(
+--id int,
+--naziv varchar(50), 
+--postupak varchar(50),
+--cijena decimal (12,2)
+--);
 
-create table djelatnici(
-id int, 
-ime varchar(50),
-prezime varchar(50),
-oib char(11),
-iban varchar(50)
-);
+--create table djelatnici(
+--id int, 
+--ime varchar(50),
+--prezime varchar(50),
+--oib char(11),
+--iban varchar(50)
+--);
