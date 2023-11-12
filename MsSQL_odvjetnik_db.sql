0d9edc84a93804c22eb09f20530219cadbf9c4a0
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-12 19:16:01 +0100

    Create MsSQL_odvjetnik_db.sql

---
+++ MsSQL_odvjetnik_db.sql
@@ -0,0 +1,37 @@
+--create database odvjetnik;
+
+use odvjetnik;
+
+--create table klijenti(
+--id int, 
+--ime varchar(50),
+--prezime varchar(50),
+--postupak varchar(50)
+--);
+
+--create table postupci(
+--klijent varchar(50),
+--obrana varchar(50),
+
+--);
+
+--create table obrana(
+--id int, 
+--naziv varchar(50),
+--postupak varchar(50),
+--sudionik varchar(50),
+--brojPostupka varchar(50)
+--);
+
+--create table sudionici(
+--obrana varchar(50),
+--suradnik varchar(50)
+--);
+
+--create table suradnici(
+--id int, 
+--ime varchar (50),
+--prezime varchar(50),
+--oib char(11)
+
+--);
