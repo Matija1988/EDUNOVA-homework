b80324ff4ddd6e05a1d47850289aa542104668bc
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-19 20:43:54 +0100

    Update MsSQL_djecjaigraonica_db.sql
    Added foreign keys. Fixed datatypes

--- MsSQL_djecjaigraonica_db.sql
+++ MsSQL_djecjaigraonica_db.sql
@@ -1,32 +1,43 @@
---create database djecjaigraonica;
-
+use master; 
+go
+drop database djecjaigraonica;
+go
+create database djecjaigraonica; 
+go 
+alter database djecjaigraonica collate Croatian_CI_AS;
+go
 use djecjaigraonica;
 
---create table skupine(
---naziv varchar(50),
---grupa varchar(50),
---soba varchar(50),
---teta varchar(50)
---);
+create table skupine(
+skupineID int not null primary key identity(1,1),
+naziv varchar(50) not null,
+grupa varchar(50) not null,
+soba varchar(50),
+tetaID int
+);
 
---create table djeca(
---id int,
---ime varchar(50),
---prezime varchar(50),
---imeRoditelja varchar(50),
---prezimeRoditelja varchar(50),
---vrijemeDolaska datetime
+create table djeca(
+id int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+imeRoditelja varchar(50) not null,
+prezimeRoditelja varchar(50) not null,
+vrijemeDolaska datetime
 
---);
+);
 
---create table grupe(
---radionica varchar(50),
---djete varchar(50)
---);
+create table grupe(
+skupina int,
+djete int
+);
 
---create table tete(
---id int, 
---ime varchar(50),
---prezime varchar(50),
---oib char(11)
---);
+create table tete(
+id int not null primary key identity(1,1), 
+ime varchar(50) not null,
+prezime varchar(50) not null,
+oib char(11) not null
+);
+
+alter table grupe add foreign key (skupina) references skupine(skupineID);
+alter table grupe add foreign key (djete) references djeca(id); 
+alter table skupine add foreign key(tetaID) references tete(id); 
