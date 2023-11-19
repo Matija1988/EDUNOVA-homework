869f62a88a5ee819cabf78af5f29648a9ce499ce
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-19 21:13:28 +0100

    Update MsSQL_fakultet_db.sql

--- MsSQL_fakultet_db.sql
+++ MsSQL_fakultet_db.sql
@@ -1,29 +1,40 @@
---create database fakultet; 
+use master;
+go
+drop database fakultet;
+go
+create database fakultet;
+go
+alter database fakultet collate Croatian_CI_AS;
+go
+use fakultet;
 
-use fakultet; 
 
---create table studenti (
---id int, 
---ime varchar (20),
---prezime varchar(20),
---brStudomat varchar(20) 
---);
+create table studenti (
+id int not null primary key identity(1,1), 
+ime varchar (20) not null,
+prezime varchar(20) not null,
+brStudomat varchar(20) not null 
+);
 
---create table kolegiji(
---id int, 
---naziv varchar(50),
---pocetak datetime,
---kraj datetime
---);
+create table kolegiji(
+id int not null primary key identity(1,1), 
+naziv varchar(50) not null,
+pocetak datetime,
+kraj datetime
+);
 
---create table rokovi (
---id int, 
---vrijemeOdrzavanja datetime,
---kolegij varchar(50)
---);
+create table rokovi (
+id int not null primary key identity(1,1), 
+vrijemeOdrzavanja datetime not null,
+kolegij int not null
+);
 
---create table prijavaNaIspit(
---id int, 
---student varchar(50),
---rok varchar(50)
---);
+create table prijavaNaIspit(
+id int not null primary key identity(1,1), 
+student int not null,
+rok int not null
+);
+
+alter table prijavaNaIspit add foreign key (student) references studenti(id); 
+alter table prijavaNaIspit add foreign key (rok) references rokovi(id);
+alter table rokovi add foreign key (kolegij) references kolegiji(id);
