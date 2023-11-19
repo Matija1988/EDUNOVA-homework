d080c01459bfac0ed6965e79ef033cdd3de499cc
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-19 21:29:57 +0100

    Update MsSQL_klubCitateljaKnjiga.sql

--- MsSQL_klubCitateljaKnjiga.sql
+++ MsSQL_klubCitateljaKnjiga.sql
@@ -1,38 +1,48 @@
---create database klubcitateljaknjiga;
-
+use master;
+go
+drop database klubcitateljaknjiga;
+go
+create database klubcitateljaknjiga;
+go
+alter database klubcitateljaknjiga collate Croatian_CI_AS;
+go
 use klubcitateljaknjiga;
 
---create table citatelji(
---id int, 
---serijskiBrojIskaznice int,
---ime varchar(20),
---prezime varchar(20),
---evidencijaPosudbe varchar(50)
---);
+create table citatelji(
+id int not null primary key identity(1,1), 
+serijskiBrojIskaznice int not null,
+ime varchar(20) not null,
+prezime varchar(20) not null,
+evidencijaPosudbe int
+);
 
---create table vlasnici(
---id int,
---citatelj varchar(50),
---evidencijaVlasnistva varchar(50)
---);
+create table vlasnici(
+id int not null primary key identity(1,1),
+citatelj int,
+evidencijaVlasnistva int
+);
 
---create table knjige(
---id int,
---barkodBroj int,
---naziv varchar(50),
---autor varchar(50),
---cijena decimal (6,2)
---);
+create table knjige(
+id int not null primary key identity(1,1),
+barkodBroj int not null,
+naziv varchar(50) not null,
+autor varchar(50) not null,
+cijena decimal (6,2) not null
+);
 
---create table evidencijaVlasnistva(
---vlasnik varchar(50),
---knjiga varchar(50)
---);
+create table evidencijaVlasnistva(
+vlasnik int,
+knjiga int
+);
 
---create table evidencijaPosudivanja(
---citatelj varchar(50),
---serijskiBrojIskaznice int,
---knjiga varchar(50),
---datumPosudivanja datetime
---);
+create table evidencijaPosudivanja(
+citatelj int,
+knjiga int,
+datumPosudivanja datetime
+);
 
+alter table evidencijaVlasnistva add foreign key (vlasnik) references vlasnici(id);
+alter table evidencijaVlasnistva add foreign key (knjiga) references knjige(id);
+alter table evidencijaPosudivanja add foreign key (citatelj) references citatelji(id);
+alter table evidencijaPosudivanja add foreign key (knjiga) references knjige(id);
+alter table vlasnici add foreign key (citatelj) references citatelji(id);
