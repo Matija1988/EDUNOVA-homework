f96f5c8f81731d74ca2a0ec114ba992c74d4a618
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-21 19:35:30 +0100

    Update MsSQL_muzej.sql

--- MsSQL_muzej.sql
+++ MsSQL_muzej.sql
@@ -1,16 +1,23 @@
+use master;
+go
+drop database if exists muzej;
+go
+create database muzej;
+go
+alter database muzej collate CROATIAN_CI_AS;
+go
 use muzej;
 
 create table djela(
-ID int,
+ID int not null primary key identity(1,1),
 naziv varchar(50),
 autorIme varchar (20),
 autorPrezime varchar(20), 
-tip varchar(20),
-zaIzlozbu varchar(50)
+tip varchar(20)
 );
 
 create table sponzori (
-ID int,
+ID int not null primary key identity(1,1),
 ime varchar(20),
 prezime varchar(20),
 oib char(11),
@@ -19,7 +26,7 @@ spol char(1),
 );
 
 create table kustosi (
-ID int,
+ID int not null primary key identity(1,1),
 ime varchar(50),
 prezime varchar(50),
 iban varchar(50),
@@ -28,16 +35,21 @@ oib char(11)
 );
 
 create table grupe (
-ID int, 
-zaIzlozbu varchar(50),
-kustos varchar(50)
+djelo int,
+izlozba int
 );
 
 
 create table izlozbe(
-ID int, 
+ID int not null primary key identity(1,1), 
 naziv varchar(50), 
-grupa varchar(50),
-sponzor varchar(50),
-prostor varchar(50)
+grupa int,
+sponzor int,
+prostor varchar(50),
+kustos int
 );
+
+alter table grupe add foreign key (djelo) references djela(id);
+alter table grupe add foreign key (izlozba) references izlozbe(id); 
+alter table izlozbe add foreign key (sponzor) references sponzori(id);
+alter table izlozbe add foreign key (kustos) references kustosi(id); 
