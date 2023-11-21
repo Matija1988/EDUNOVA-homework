439bcd44443d688b0676f6d3a5a2435f27556c6f
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-21 19:43:28 +0100

    Update MsSQL_nakladnici_db.sql

--- MsSQL_nakladnici_db.sql
+++ MsSQL_nakladnici_db.sql
@@ -1,35 +1,46 @@
---create database nakladnici;
-
+use master;
+go 
+drop database if exists nakladnici;
+go
+create database nakladnici;
+go
+alter database nakladnici collate croatian_ci_as;
+go
 use nakladnici;
 
---create table nakladnici (
---id int, 
---naziv varchar(50),
---lokacija varchar(50),
---ponuda varchar(50)
+create table nakladnici (
+id int not null primary key identity(1,1), 
+naziv varchar(50),
+lokacija int,
+ponuda int
 
---);
+);
 
---create table lokacije(
---nakladnik varchar(50),
---mjesto varchar(50)
---);
+create table lokacije(
+nakladnik int,
+mjesto int
+);
 
---create table mjesto(
---adresa varchar(50),
---naziv varchar(50),
---koordinati varchar(50)
---);
+create table mjesta(
+id int not null primary key identity(1,1),
+adresa varchar(50),
+naziv varchar(50),
+koordinati varchar(50)
+);
 
---create table djela(
---id int, 
---naziv varchar(50),
---autor varchar(50),
---cijena decimal(6,2)
---);
+create table djela(
+id int not null primary key identity(1,1), 
+naziv varchar(50),
+autor varchar(50),
+cijena decimal(8,2)
+);
 
---create table ponuda(
---nakladnik varchar(50),
---djelo varchar(50)
---);
+create table ponuda(
+nakladnik int,
+djelo int
+);
 
+alter table ponuda add foreign key (nakladnik) references nakladnici(id);
+alter table ponuda add foreign key (djelo) references djela(id);
+alter table lokacije add foreign key (nakladnik) references nakladnici(id); 
+alter table lokacije add foreign key (mjesto) references mjesta(id); 
