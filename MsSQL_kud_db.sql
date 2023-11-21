d6f28b34ec000529a22f6bdb2c2f55f2f504b47b
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-21 19:23:21 +0100

    Update MsSQL_kud_db.sql

--- MsSQL_kud_db.sql
+++ MsSQL_kud_db.sql
@@ -1,34 +1,42 @@
---create database kud;
-
+use master;
+go
+drop database if exists kud;
+go
+create database kud;
+go
+alter database kud collate CROATIAN_CI_AS;
+go
 use kud;
 
---create table nastupi(
---id int, 
---mjesto varchar(50),
---naziv varchar(50),
---skupina varchar(50)
---);
+create table nastupi(
+id int not null primary key identity(1,1), 
+mjesto int,
+naziv varchar(50),
+skupina int
+);
 
---create table mjesta(
---adresa varchar(50),
---naziv varchar(50),
---koordinati varchar(50),
---inozemstvo bit
---);
+create table mjesta(
+id int not null primary key identity(1,1),
+adresa varchar(50) not null,
+naziv varchar(50) not null,
+koordinati varchar(50),
+inozemstvo bit 
+);
 
---create table clanovi(
---id int, 
---ime varchar(50),
---prezime varchar(50),
---oib char(11),
---datumIstekaPutovnice datetime,
---iban varchar(50)
---);
+create table clanovi(
+id int not null primary key identity(1,1), 
+ime varchar(50) not null,
+prezime varchar(50) not null,
+oib char(11),
+datumIstekaPutovnice datetime,
+iban varchar(50)
+);
 
---create table skupina(
---clan varchar(50),
---nastup varchar(50)
---);
+create table skupine(
+clan int,
+nastup int
+);
 
-
-
+alter table skupine add foreign key (clan) references clanovi(id); 
+alter table skupine add foreign key (nastup) references nastupi(id);
+alter table nastupi add foreign key (mjesto) references mjesta(id); 
