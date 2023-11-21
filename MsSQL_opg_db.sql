3d4b05fb1e8baf11c84e743db0f35121436d38fd
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-21 19:57:14 +0100

    Update MsSQL_opg_db.sql

--- MsSQL_opg_db.sql
+++ MsSQL_opg_db.sql
@@ -1,30 +1,40 @@
---create database opg;
-
+use master;
+go
+drop database if exists opg;
+go
+create database opg;
+go
+alter database opg collate croatian_ci_as;
+go
 use opg;
 
---create table sirovine(
---id int, 
---naziv varchar(50),
---cijena decimal (12,2)
---);
+create table sirovine(
+id int not null primary key identity(1,1), 
+naziv varchar(50),
+cijena decimal (12,2)
+);
 
---create table postupci (
---sirovina varchar(50),
---proizvod varchar(50),
---djelatnik varchar(50)
---);
+create table postupci (
+sirovina int,
+proizvod int,
+djelatnik int
+);
 
---create table proizvodi(
---id int,
---naziv varchar(50), 
---postupak varchar(50),
---cijena decimal (12,2)
---);
+create table proizvodi(
+id int not null primary key identity(1,1),
+naziv varchar(50), 
+postupak varchar(50),
+cijena decimal (12,2)
+);
 
---create table djelatnici(
---id int, 
---ime varchar(50),
---prezime varchar(50),
---oib char(11),
---iban varchar(50)
---);
+create table djelatnici(
+id int not null primary key identity(1,1), 
+ime varchar(50) not null,
+prezime varchar(50) not null,
+oib char(11) not null,
+iban varchar(50)
+);
+
+alter table postupci add foreign key (sirovina) references sirovine(id);
+alter table postupci add foreign key (proizvod) references proizvodi(id);
+alter table postupci add foreign key (djelatnik) references djelatnici(id); 
