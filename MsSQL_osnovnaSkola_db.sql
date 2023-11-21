fe7d8d276594ef186c162783849795c329f78163
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-21 20:04:07 +0100

    Update MsSQL_osnovnaSkola_db.sql

--- MsSQL_osnovnaSkola_db.sql
+++ MsSQL_osnovnaSkola_db.sql
@@ -1,35 +1,40 @@
---create database osnovnaskola; 
+use master;
+go
+drop database if exists osnovnaskola;
+go
+create database osnovnaskola;
+go
+alter database osnovnaskola collate croatian_CI_AS;
+go 
 use osnovnaskola;
 
---create table djeca(
---id int, 
---ime varchar(50),
---prezime varchar(50),
---spol char(1)
---);
+create table djeca(
+id int not null primary key identity(1,1), 
+ime varchar(50),
+prezime varchar(50),
+spol char(1)
+);
 
---create table uciteljice(
---id int, 
---ime varchar(20),
---prezime varchar(20),
+create table uciteljice(
+id int not null primary key identity(1,1), 
+ime varchar(20),
+prezime varchar(20),
 
---);
+);
 
---create table radionice(
---id int, 
---naziv varchar(50),
---tema varchar(50),
---grupa varchar(50),
---uciteljica varchar(50)
---); 
+create table radionice(
+id int not null primary key identity(1,1), 
+naziv varchar(50),
+tema varchar(50),
+grupa int,
+uciteljica int
+); 
 
---create table grupa(
---id int, 
---naziv varchar(50),
---dijete varchar(50),
---radionica varchar(50)
---);
+create table grupa(
+dijete int,
+radionica int
+);
 
---alter table grupa drop column radionica;
-
---alter table djeca add razred varchar(50); 
+alter table grupa add foreign key (dijete) references djeca(id);
+alter table grupa add foreign key (radionica) references radionice(id);
+alter table radionice add foreign key(uciteljica) references uciteljice(id); 
