cc29612302836a711dd74c518d2bdc0f269b2558
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2023-11-21 19:15:15 +0100

    Update MsSQL_kozmetickisalon_db.sql

--- MsSQL_kozmetickisalon_db.sql
+++ MsSQL_kozmetickisalon_db.sql
@@ -1,30 +1,44 @@
-----create database kozmeticki;
---use kozmeticki;
+use master;
+go 
+drop database if exists kozmeticki;
+go
+create database kozmeticki;
+go 
+alter database kozmeticki collate CROATIAN_CI_AS; 
+go 
+use kozmeticki;
 
---create table posjeta(
---korisnik varchar(50),
---termin datetime, 
---djelatnik varchar(50),
---usluga varchar(50)
---);
 
---create table korisnici (
---brmobitel varchar(50),
---spol char(1),
---tipkoze varchar(50),
---dob int, 
---napomena varchar(100)
---);
+create table posjeta(
+id int not null primary key identity(1,1),
+korisnik int,
+termin datetime, 
+djelatnik int,
+usluga int
+);
 
---create table djelatnici (
---ID int,
---ime varchar (50),
---prezime varchar(50),
---specijaliziranza varchar(50)
---);
+create table korisnici (
+id int not null primary key identity(1,1), 
+brmobitel varchar(50),
+spol char(1),
+tipkoze varchar(50),
+dob int, 
+napomena varchar(100)
+);
 
---create table usluga (
---ID int,
---naziv varchar (50),
---cijena decimal (10,2)
---);
+create table djelatnici (
+ID int not null primary key identity(1,1),
+ime varchar (50),
+prezime varchar(50),
+specijaliziranZa varchar(50)
+);
+
+create table usluge (
+ID int not null primary key identity(1,1),
+naziv varchar (50),
+cijena decimal (10,2)
+);
+
+alter table posjeta add foreign key (korisnik) references korisnici(id); 
+alter table posjeta add foreign key (djelatnik) references djelatnici(id);
+alter table posjeta add foreign key (usluga) references usluge(id);
